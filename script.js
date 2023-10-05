51decd920482057695a482145439ab925d6811d3
Andrew <145285546+Feud-Rose@users.noreply.github.com>
2023-10-05 13:47:41 -0600

    Assign rps string too the random number generated

--- script.js
+++ script.js
@@ -3,14 +3,21 @@ console.log("Hello World");
 
 //Function called getComputerChoice
 //Generate a random number from 1-3
-
-
 function getComputerChoice() {
-   return Math.floor(Math.random()*(3)+1);
+  let ranNum = Math.floor(Math.random()*(3)+1);
+    console.log(ranNum) 
+    //Assign rock paper or scissors based on number selected
+    if (ranNum === 1) {
+        return "Rock"
+    }
+    else if (ranNum === 2) {
+        return "Paper"
+    }
+    else {
+        return "Scissors"
+    }
 }
-
 let comRoll = getComputerChoice()
-
 console.log(comRoll)
 
 //Assign rock paper or scissors based on number selected
