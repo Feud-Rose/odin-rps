ee19e9e65b22366386fc21462d1262365f4cc933
Andrew <145285546+Feud-Rose@users.noreply.github.com>
2023-10-05 14:57:15 -0600

    Add function too determine winner

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ function getComputerChoice() {
         return "Scissors"
     }
 }
-let comRoll = getComputerChoice()
-console.log(comRoll)
+let computerSelection = getComputerChoice()
+console.log(computerSelection)
 
 
 
@@ -26,12 +26,52 @@ console.log(comRoll)
 //Convert to all CAPS
 let choice = prompt("Please type Rock, Paper or Scissors!", "Type Here");
 console.log(choice)
-let playerSelection = choice.toUpperCase();
+let playerSelection = choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
 console.log(playerSelection)
 
 //Compare Player and Comp Selections
 
+function playGame(playerSelection, computerSelection) {
+    if (playerSelection === "Rock") {
+        if (computerSelection === "Rock") {
+        return "Draw you both picked " + computerSelection + ".";
+        }
+        else if (computerSelection === "Paper") {
+        return "You Lose! " + computerSelection + " beats " + playerSelection + ".";
+        }
+        else {
+        return "You Win! " + playerSelection + " beats " + computerSelection + ".";
+        }
+    }
+    else if (playerSelection === "Paper") {
+        if (computerSelection === "Rock") {
+        return "You Win! " + playerSelection + " beats " + computerSelection + ".";
+        }
+        else if (computerSelection === "Paper") {
+        return "Draw you both picked " + computerSelection + "."
+        }
+        else {
+        return "You Lose! " + computerSelection + " beats " + playerSelection + ".";
+        }
+    }
+    else if (playerSelection === "Scissors") {
+        if (computerSelection === "Rock") {
+        return "You Lose! " + computerSelection + " beats " + playerSelection + ".";
+        }
+        else if (computerSelection === "Paper") {
+        return "You Win! " + playerSelection + " beats " + computerSelection + ".";
+        }
+        else {
+        return "Draw you both picked " + computerSelection + "."
+        }
+    }
+    else {
+        return playerSelection + " is not a valid choice.";
+    }
+    
 
 
-
+}
+console.log(playGame(playerSelection, computerSelection));
+//computerSelection
 //Announce Results
