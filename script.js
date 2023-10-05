7f1013430dbb15a805ae8d3fb8303d1e50150da9
Andrew <145285546+Feud-Rose@users.noreply.github.com>
2023-10-05 13:40:32 -0600

    Add random number gen for com opponent

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("Hello World");
+
+
+//Function called getComputerChoice
+//Generate a random number from 1-3
+
+
+function getComputerChoice() {
+   return Math.floor(Math.random()*(3)+1);
+}
+
+let comRoll = getComputerChoice()
+
+console.log(comRoll)
+
+//Assign rock paper or scissors based on number selected
